58fc034eda8317417bb9e6363d6adeaa336c94b5
Jonathan S <gereeter+code@gmail.com>
2015-06-12 15:54:36 -0500

    Add some TODOs to the LLVM wrapper

--- src/llvm.rs
+++ src/llvm.rs
@@ -6,6 +6,27 @@ use llvm_sys::core::*;
 
 use id::{Id, IdRef};
 
+//
+// TODO: Error Checking
+//   This is currently a safety hole.
+//
+// TODO: Reduce the number of lifetimes.
+//   It isn't completely clear that this is possible, but this is a first attempt.
+//
+// TODO: Docs
+//
+// TODO: See what safety guarentees LLVM actually needs.
+//   The official LLVM docs aren't very clear on, e.g., when different contexts/modules/function/builders
+//   can be mixed, or what things can outlive other things.
+//
+// TODO: Consider wrapping the C++ API in another way.
+//   The official wrapper is useful, since it requires no effort and is maintained by LLVM. However,
+//   it has a number of annoying issues. For example, given the similarity between LLVM's Twine and Rust's
+//   &str, there should be no reason to use CStr.
+//
+// TODO: Missing functionality
+//
+
 pub struct Context<'cid> {
     _id: Id<'cid>,
     llvm_context: LLVMContextRef
