6ce1ff4f92399867ee20f8bfa2fe26918cfefb61
Jonathan S <gereeter+code@gmail.com>
2015-11-01 11:29:35 -0600

    Add another function to main to demonstrate the use of the FunctionBuilder

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,15 @@ fn main() {
         let context = llvm::Context::new(context_id);
         id::with(|module_id| {
             let module = llvm::Module::new(module_id, &CString::new("mymodule").unwrap(), &context);
+
+            let i32_ty = llvm::Type::i32(&context);
+            let func_ty = llvm::Type::function(&[i32_ty], i32_ty);
+            let mut builder = llvm::Builder::new(&context);
+
             id::with(|function_id| {
-                let i32_ty = llvm::Type::i32(&context);
-                let func_ty = llvm::Type::function(&[i32_ty], i32_ty);
                 let mut function = module.add_function(function_id, &CString::new("square").unwrap(), func_ty);
                 {
                     let mut function_builder = function.builder();
-                    let mut builder = llvm::Builder::new(&context);
 
                     let entry = function_builder.append_basic_block(&CString::new("entry").unwrap(), &context);
                     let builder = builder.position_at_end(entry);
@@ -29,6 +31,20 @@ fn main() {
                 }
 
                 function.dump();
+            });
+
+            id::with(|function_id| {
+                let mut function = module.add_function(function_id, &CString::new("jumpy").unwrap(), func_ty);
+                {
+                    let mut function_builder = function.builder();
+                    let entry = function_builder.append_basic_block(&CString::new("entry").unwrap(), &context);
+                    let exit = function_builder.append_basic_block(&CString::new("exit").unwrap(), &context);
+
+                    builder.position_at_end(entry).br(exit);
+                    builder.position_at_end(exit).ret(function_builder.param(0));
+                }
+
+                function.dump()
             });
         });
     });
